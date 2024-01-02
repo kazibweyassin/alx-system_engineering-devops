95a6a0947de2101859683f5ebb621bbf35aaa0be
kazibweyassin <kazibweusama@gmail.com>
2024-01-02 23:51:01 +0300

    new

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,27 +1,34 @@
-#!/usr/bin/env python3
-"""Gather data from an API"""
+#!/usr/bin/python3
+"""Get the todo list for one user and print info"""
 
 import requests
 from sys import argv
 
-def main():
-    """Gather data from an API"""
-    
-    userID = argv[1]
-    user = 'https://jsonplaceholder.typicode.com/users/{}'.format(userID)
-    todo = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(userID)
-    name = requests.get(user).json().get('name')
-    tasks = requests.get(todo).json()
-    total_tasks = len(tasks)
-    completed_tasks = 0
-    for task in tasks:
-        if task.get('completed') is True:
-            completed_tasks += 1
-    print('Employee {} is done with tasks({}/{}):'.format(name, completed_tasks, total_tasks))
-    for task in tasks:
-        if task.get('completed') is True:
-            print('\t {}'.format(task.get('title')))
-            
 if __name__ == "__main__":
-    if len(argv) == 2:
-        main()
+    try:
+        user_id = argv[1]
+    except IndexError:
+        print("Please provide a user ID as a command-line argument.")
+        exit()
+
+    url = "https://jsonplaceholder.typicode.com/todos?userId=" + user_id
+    resp = requests.get(url)
+    tasks = resp.json()
+    url = "https://jsonplaceholder.typicode.com/users/" + user_id
+    resp = requests.get(url)
+    user = resp.json()
+
+    total = 0
+    done = 0
+    for task in tasks:
+        if task.get("completed"):
+            done = done + 1
+        total = total + 1
+
+    tasks = [t for t in tasks if t.get("completed")]
+    name = user.get("name")
+    if name is None:
+        exit()
+    print("Employee {} is done with tasks({}/{}):".format(name, done, total))
+    for task in tasks:
+        print("\t " + task.get("title"))
